c07fc0608fdcfc3c39391b930fb19d56c1457e6a
sysnav_hisanaga <sysnav.hisanaga@gmail.com>
2019-07-29 10:57:43 +0900

    受入ソース(研磨・計数)アップ

---
+++ src/java/jp/co/kccs/xhd/db/model/SrSyoseikeisuu.java
@@ -0,0 +1,389 @@
+/*
+ * Copyright 2019 Kyocera Communication Systems Co., Ltd All rights reserved.
+ */
+package jp.co.kccs.xhd.db.model;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+/**
+ * ===============================================================================<br>
+ * <br>
+ * システム名	品質DB(コンデンサ)<br>
+ * <br>
+ * 変更日	2019/07/22<br>
+ * 計画書No	K1803-DS001<br>
+ * 変更者	KCSS K.Jo<br>
+ * 変更理由	新規作成<br>
+ * <br>
+ * ===============================================================================<br>
+ */
+/**
+ * SR_SYOSEIKEISUU(計数)のモデルクラスです。
+ *
+ * @author KCSS K.Jo
+ * @since  2019/07/22
+ */
+public class SrSyoseikeisuu {
+    /**
+     * 工場ｺｰﾄﾞ
+     */
+    private String kojyo;
+
+    /**
+     * ﾛｯﾄNo
+     */
+    private String lotno;
+
+    /**
+     * 枝番
+     */
+    private String edaban;
+
+    /**
+     * 実績No
+     */
+    private Integer jissekino;
+
+    /**
+     * KCPNO
+     */
+    private String kcpno;
+
+    /**
+     * 受入個数
+     */
+    private Integer ukeirekosuu;
+
+    /**
+     * 単位重量
+     */
+    private BigDecimal tanijyuryo;
+
+    /**
+     * 総重量
+     */
+    private BigDecimal soujuryou;
+
+    /**
+     * 良品個数
+     */
+    private Integer ryohinkosuu;
+
+    /**
+     * 計数日時
+     */
+    private Timestamp keinichiji;
+
+    /**
+     * 計数担当者
+     */
+    private String keitantosya;
+
+    /**
+     * 歩留まり
+     */
+    private BigDecimal budomari;
+
+    /**
+     * 備考1
+     */
+    private String biko1;
+
+    /**
+     * 備考2
+     */
+    private String biko2;
+
+    /**
+     * ﾊﾞﾚﾙ後判定
+     */
+    private String barrelgohantei;
+
+    /**
+     * 登録日時
+     */
+    private Timestamp torokunichiji;
+
+    /**
+     * 更新日時
+     */
+    private Timestamp kosinNichiji;
+
+    /**
+     * revision
+     */
+    private Integer revision;
+
+    /**
+     * 削除ﾌﾗｸﾞ
+     */
+    private Integer deleteflag;
+
+    /**
+     * @return kojyo
+     */
+    public String getKojyo() {
+        return kojyo;
+    }
+
+    /**
+     * @param kojyo セットする kojyo
+     */
+    public void setKojyo(String kojyo) {
+        this.kojyo = kojyo;
+    }
+
+    /**
+     * @return lotno
+     */
+    public String getLotno() {
+        return lotno;
+    }
+
+    /**
+     * @param lotno セットする lotno
+     */
+    public void setLotno(String lotno) {
+        this.lotno = lotno;
+    }
+
+    /**
+     * @return edaban
+     */
+    public String getEdaban() {
+        return edaban;
+    }
+
+    /**
+     * @param edaban セットする edaban
+     */
+    public void setEdaban(String edaban) {
+        this.edaban = edaban;
+    }
+
+    /**
+     * @return jissekino
+     */
+    public Integer getJissekino() {
+        return jissekino;
+    }
+
+    /**
+     * @param jissekino セットする jissekino
+     */
+    public void setJissekino(Integer jissekino) {
+        this.jissekino = jissekino;
+    }
+
+    /**
+     * @return kcpno
+     */
+    public String getKcpno() {
+        return kcpno;
+    }
+
+    /**
+     * @param kcpno セットする kcpno
+     */
+    public void setKcpno(String kcpno) {
+        this.kcpno = kcpno;
+    }
+
+    /**
+     * @return ukeirekosuu
+     */
+    public Integer getUkeirekosuu() {
+        return ukeirekosuu;
+    }
+
+    /**
+     * @param ukeirekosuu セットする ukeirekosuu
+     */
+    public void setUkeirekosuu(Integer ukeirekosuu) {
+        this.ukeirekosuu = ukeirekosuu;
+    }
+
+    /**
+     * @return tanijyuryo
+     */
+    public BigDecimal getTanijyuryo() {
+        return tanijyuryo;
+    }
+
+    /**
+     * @param tanijyuryo セットする tanijyuryo
+     */
+    public void setTanijyuryo(BigDecimal tanijyuryo) {
+        this.tanijyuryo = tanijyuryo;
+    }
+
+    /**
+     * @return soujuryou
+     */
+    public BigDecimal getSoujuryou() {
+        return soujuryou;
+    }
+
+    /**
+     * @param soujuryou セットする soujuryou
+     */
+    public void setSoujuryou(BigDecimal soujuryou) {
+        this.soujuryou = soujuryou;
+    }
+
+    /**
+     * @return ryohinkosuu
+     */
+    public Integer getRyohinkosuu() {
+        return ryohinkosuu;
+    }
+
+    /**
+     * @param ryohinkosuu セットする ryohinkosuu
+     */
+    public void setRyohinkosuu(Integer ryohinkosuu) {
+        this.ryohinkosuu = ryohinkosuu;
+    }
+
+    /**
+     * @return keinichiji
+     */
+    public Timestamp getKeinichiji() {
+        return keinichiji;
+    }
+
+    /**
+     * @param keinichiji セットする keinichiji
+     */
+    public void setKeinichiji(Timestamp keinichiji) {
+        this.keinichiji = keinichiji;
+    }
+
+    /**
+     * @return keitantosya
+     */
+    public String getKeitantosya() {
+        return keitantosya;
+    }
+
+    /**
+     * @param keitantosya セットする keitantosya
+     */
+    public void setKeitantosya(String keitantosya) {
+        this.keitantosya = keitantosya;
+    }
+
+    /**
+     * @return budomari
+     */
+    public BigDecimal getBudomari() {
+        return budomari;
+    }
+
+    /**
+     * @param budomari セットする budomari
+     */
+    public void setBudomari(BigDecimal budomari) {
+        this.budomari = budomari;
+    }
+
+    /**
+     * @return biko1
+     */
+    public String getBiko1() {
+        return biko1;
+    }
+
+    /**
+     * @param biko1 セットする biko1
+     */
+    public void setBiko1(String biko1) {
+        this.biko1 = biko1;
+    }
+
+    /**
+     * @return biko2
+     */
+    public String getBiko2() {
+        return biko2;
+    }
+
+    /**
+     * @param biko2 セットする biko2
+     */
+    public void setBiko2(String biko2) {
+        this.biko2 = biko2;
+    }
+
+    /**
+     * @return barrelgohantei
+     */
+    public String getBarrelgohantei() {
+        return barrelgohantei;
+    }
+
+    /**
+     * @param barrelgohantei セットする barrelgohantei
+     */
+    public void setBarrelgohantei(String barrelgohantei) {
+        this.barrelgohantei = barrelgohantei;
+    }
+
+    /**
+     * @return torokunichiji
+     */
+    public Timestamp getTorokunichiji() {
+        return torokunichiji;
+    }
+
+    /**
+     * @param torokunichiji セットする torokunichiji
+     */
+    public void setTorokunichiji(Timestamp torokunichiji) {
+        this.torokunichiji = torokunichiji;
+    }
+
+    /**
+     * @return kosinNichiji
+     */
+    public Timestamp getKosinNichiji() {
+        return kosinNichiji;
+    }
+
+    /**
+     * @param kosinNichiji セットする kosinNichiji
+     */
+    public void setKosinNichiji(Timestamp kosinNichiji) {
+        this.kosinNichiji = kosinNichiji;
+    }
+
+    /**
+     * @return revision
+     */
+    public Integer getRevision() {
+        return revision;
+    }
+
+    /**
+     * @param revision セットする revision
+     */
+    public void setRevision(Integer revision) {
+        this.revision = revision;
+    }
+
+    /**
+     * @return deleteflag
+     */
+    public Integer getDeleteflag() {
+        return deleteflag;
+    }
+
+    /**
+     * @param deleteflag セットする deleteflag
+     */
+    public void setDeleteflag(Integer deleteflag) {
+        this.deleteflag = deleteflag;
+    }
+
+}
---
+++ src/java/jp/co/kccs/xhd/pxhdo101/GXHDO101B021Const.java
@@ -0,0 +1,184 @@
+/*
+ * Copyright 2019 Kyocera Communication Systems Co., Ltd All rights reserved.
+ */
+package jp.co.kccs.xhd.pxhdo101;
+
+/**
+ * ===============================================================================<br>
+ * <br>
+ * システム名	品質DB(コンデンサ)<br>
+ * <br>
+ * 変更日	2019/07/22<br>
+ * 計画書No	K1803-DS001<br>
+ * 変更者	KCSS K.Jo<br>
+ * 変更理由	新規作成<br>
+ * <br>
+ * ===============================================================================<br>
+ */
+/**
+ * GXHDO101B021Const(研磨・計数)定数
+ *
+ * @author KCSS K.Jo
+ * @since  2019/07/22
+ */
+public class GXHDO101B021Const {
+    /**
+     * ﾛｯﾄNo.
+     */
+    public static final String LOTNO = "sr_syoseikeisuu_lotno";
+
+    /**
+     * KCPNO
+     */
+    public static final String KCPNO = "sr_syoseikeisuu_kcpno";
+
+    /**
+     * 客先
+     */
+    public static final String KYAKUSAKI = "sr_syoseikeisuu_kyakusaki";
+
+    /**
+     * ﾛｯﾄ区分
+     */
+    public static final String LOT_KUBUN = "sr_syoseikeisuu_lot_kubun";
+
+    /**
+     * ｵｰﾅｰ
+     */
+    public static final String OWNER = "sr_syoseikeisuu_owner";
+
+    /**
+     * 指示
+     */
+    public static final String SIJI = "sr_syoseikeisuu_siji";
+
+    /**
+     * 受入個数
+     */
+    public static final String UKEIREKOSUU = "sr_syoseikeisuu_ukeirekosuu";
+
+    /**
+     * 単位重量
+     */
+    public static final String TANIJURYO = "sr_syoseikeisuu_tanijuryo";
+
+    /**
+     * 総重量
+     */
+    public static final String SOJURYO = "sr_syoseikeisuu_sojuryo";
+
+    /**
+     * 良品個数
+     */
+    public static final String RYOHINKOSU = "sr_syoseikeisuu_ryohinkosu";
+
+    /**
+     * 計数日
+     */
+    public static final String KEISUU_DAY = "sr_syoseikeisuu_keisuu_day";
+
+    /**
+     * 計数時間
+     */
+    public static final String KEISUU_TIME = "sr_syoseikeisuu_keisuu_time";
+
+    /**
+     * 計数担当者
+     */
+    public static final String KEISUU_TANTOUSYA = "sr_syoseikeisuu_keisuu_tantousya";
+
+    /**
+     * 歩留まり
+     */
+    public static final String BUDOMARI = "sr_syoseikeisuu_budomari";
+
+    /**
+     * 備考1
+     */
+    public static final String BIKO1 = "sr_syoseikeisuu_biko1";
+
+    /**
+     * 備考2
+     */
+    public static final String BIKO2 = "sr_syoseikeisuu_biko2";
+
+    /**
+     * 仮登録(画面上)
+     */
+    public static final String BTN_KARI_TOUROKU_TOP = "sr_syoseikeisuu_kari_touroku_Top";
+
+    /**
+     * 枝番コピー(画面上)
+     */
+    public static final String BTN_COPY_EDABAN_TOP = "sr_syoseikeisuu_copy_edaban_Top";
+
+    /**
+     * 登録(画面上)
+     */
+    public static final String BTN_INSERT_TOP = "sr_syoseikeisuu_insert_Top";
+
+    /**
+     * 修正(画面上)
+     */
+    public static final String BTN_UPDATE_TOP = "sr_syoseikeisuu_update_Top";
+
+    /**
+     * 削除(画面上)
+     */
+    public static final String BTN_DELETE_TOP = "sr_syoseikeisuu_delete_Top";
+
+    /**
+     * 計数日時(画面上)
+     */
+    public static final String BTN_KEISUUDATETIME_TOP = "sr_syoseikeisuu_keisuudatetime_Top";
+
+    /**
+     * 歩留まり計算(画面上)
+     */
+    public static final String BTN_BUDOMARI_KEISAN_TOP = "sr_syoseikeisuu_budomari_keisan_Top";
+
+    /**
+     * 仮登録(画面下)
+     */
+    public static final String BTN_KARI_TOUROKU_BOTTOM = "sr_syoseikeisuu_kari_touroku_Bottom";
+
+    /**
+     * 枝番コピー(画面下)
+     */
+    public static final String BTN_COPY_EDABAN_BOTTOM = "sr_syoseikeisuu_copy_edaban_Bottom";
+
+    /**
+     * 登録(画面下)
+     */
+    public static final String BTN_INSERT_BOTTOM = "sr_syoseikeisuu_insert_Bottom";
+
+    /**
+     * 修正(画面下)
+     */
+    public static final String BTN_UPDATE_BOTTOM = "sr_syoseikeisuu_update_Bottom";
+
+    /**
+     * 削除(画面下)
+     */
+    public static final String BTN_DELETE_BOTTOM = "sr_syoseikeisuu_delete_Bottom";
+
+    /**
+     * 計数日時(画面下)
+     */
+    public static final String BTN_KEISUUDATETIME_BOTTOM = "sr_syoseikeisuu_keisuudatetime_Bottom";
+
+    /**
+     * 歩留まり計算(画面下)
+     */
+    public static final String BTN_BUDOMARI_KEISAN_BOTTOM = "sr_syoseikeisuu_budomari_keisan_Bottom";
+
+    /**
+     * ユーザー認証パラメータ(修正)
+     */
+    public static final String USER_AUTH_UPDATE_PARAM = "sr_syoseikeisuu_update_button";
+
+    /**
+     * ユーザー認証パラメータ(削除)
+     */
+    public static final String USER_AUTH_DELETE_PARAM = "sr_syoseikeisuu_delete_button";
+}
