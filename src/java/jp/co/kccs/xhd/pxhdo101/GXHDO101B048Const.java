f3600defb32de7a9af605e3f894e13c2a1ae5e4f
sysnav_hisanaga <sysnav.hisanaga@gmail.com>
2020-02-04 19:55:24 +0900

    テーピングTP作業新規作成

---
+++ src/java/jp/co/kccs/xhd/pxhdo101/GXHDO101B048Const.java
@@ -0,0 +1,722 @@
+/*
+ * Copyright 2020 Kyocera Communication Systems Co., Ltd All rights reserved.
+ */
+package jp.co.kccs.xhd.pxhdo101;
+
+/**
+ * ===============================================================================<br>
+ * <br>
+ * システム名	品質DB(コンデンサ)<br>
+ * <br>
+ * 変更日	2020/02/03<br>
+ * 計画書No	K1811-DS001<br>
+ * 変更者	SYSNAVI K.Hisanaga<br>
+ * 変更理由	新規作成<br>
+ * <br>
+ * ===============================================================================<br>
+ */
+/**
+ * GXHDO101B048(TP作業)ロジック定数
+ *
+ * @author SYSNAVI K.Hisanaga
+ * @since 2020/02/03
+ */
+public class GXHDO101B048Const {
+
+    /**
+     * ﾛｯﾄNo.
+     */
+    public static final String LOTNO = "sr_taping_sagyo_lotno";
+
+    /**
+     * KCPNO
+     */
+    public static final String KCPNO = "sr_taping_sagyo_kcpno";
+
+    /**
+     * 客先
+     */
+    public static final String TOKUISAKI = "sr_taping_sagyo_tokuisaki";
+
+    /**
+     * ﾛｯﾄ区分
+     */
+    public static final String LOT_KUBUN = "sr_taping_sagyo_lot_kubun";
+
+    /**
+     * ｵｰﾅｰ
+     */
+    public static final String OWNER = "sr_taping_sagyo_owner";
+
+    /**
+     * 送り良品数
+     */
+    public static final String OKURI_RYOHINSU = "sr_taping_sagyo_okuri_ryohinsu";
+
+    /**
+     * 受入れ単位重量
+     */
+    public static final String UKEIRE_TANNIJURYO = "sr_taping_sagyo_ukeire_tannijuryo";
+
+    /**
+     * 受入れ総重量
+     */
+    public static final String UKEIRE_SOUJURYO = "sr_taping_sagyo_ukeire_soujuryo";
+
+    /**
+     * 検査場所
+     */
+    public static final String KENSA_BASHO = "sr_taping_sagyo_kensa_basho";
+
+    /**
+     * 号機
+     */
+    public static final String GOKI = "sr_taping_sagyo_goki";
+
+    /**
+     * TP仕様
+     */
+    public static final String TP_SHIYO = "sr_taping_sagyo_tp_shiyo";
+
+    /**
+     * 確保数
+     */
+    public static final String KAKUHOSU = "sr_taping_sagyo_kakuhosu";
+
+    /**
+     * 確保ﾘｰﾙ巻数①
+     */
+    public static final String KAKUHO_REEL_MAKISU1 = "sr_taping_sagyo_kakuho_reel_makisu1";
+
+    /**
+     * 確保ﾘｰﾙ本数①
+     */
+    public static final String KAKUHO_REEL_HONSU1 = "sr_taping_sagyo_kakuho_reel_honsu1";
+
+    /**
+     * 確保ﾘｰﾙ巻数②
+     */
+    public static final String KAKUHO_REEL_MAKISU2 = "sr_taping_sagyo_kakuho_reel_makisu2";
+
+    /**
+     * 確保ﾘｰﾙ本数②
+     */
+    public static final String KAKUHO_REEL_HONSU2 = "sr_taping_sagyo_kakuho_reel_honsu2";
+
+    /**
+     * ｷｬﾘｱﾃｰﾌﾟLOT NO.①
+     */
+    public static final String CARRIERTAPE_LOTNO1 = "sr_taping_sagyo_carriertape_lotno1";
+
+    /**
+     * ｷｬﾘｱﾃｰﾌﾟLOT NO.②
+     */
+    public static final String CARRIERTAPE_LOTNO2 = "sr_taping_sagyo_carriertape_lotno2";
+
+    /**
+     * ｷｬﾘｱﾃｰﾌﾟLOT NO.③
+     */
+    public static final String CARRIERTAPE_LOTNO3 = "sr_taping_sagyo_carriertape_lotno3";
+
+    /**
+     * ｷｬﾘｱﾃｰﾌﾟLOT NO.④
+     */
+    public static final String CARRIERTAPE_LOTNO4 = "sr_taping_sagyo_carriertape_lotno4";
+
+    /**
+     * ｷｬﾘｱﾃｰﾌﾟLOT NO.⑤
+     */
+    public static final String CARRIERTAPE_LOTNO5 = "sr_taping_sagyo_carriertape_lotno5";
+
+    /**
+     * ｷｬﾘｱﾃｰﾌﾟLOT NO.⑥
+     */
+    public static final String CARRIERTAPE_LOTNO6 = "sr_taping_sagyo_carriertape_lotno6";
+
+    /**
+     * ｷｬﾘｱﾃｰﾌﾟLOT NO.⑦
+     */
+    public static final String CARRIERTAPE_LOTNO7 = "sr_taping_sagyo_carriertape_lotno7";
+
+    /**
+     * ｷｬﾘｱﾃｰﾌﾟLOT NO.⑧
+     */
+    public static final String CARRIERTAPE_LOTNO8 = "sr_taping_sagyo_carriertape_lotno8";
+
+    /**
+     * ｷｬﾘｱﾃｰﾌﾟLOT NO.⑨
+     */
+    public static final String CARRIERTAPE_LOTNO9 = "sr_taping_sagyo_carriertape_lotno9";
+
+    /**
+     * ｷｬﾘｱﾃｰﾌﾟLOT NO.⑩
+     */
+    public static final String CARRIERTAPE_LOTNO10 = "sr_taping_sagyo_carriertape_lotno10";
+
+    /**
+     * ﾄｯﾌﾟﾃｰﾌﾟLOT NO.①
+     */
+    public static final String TOPTAPE_LOTNO1 = "sr_taping_sagyo_toptape_lotno1";
+
+    /**
+     * ﾄｯﾌﾟﾃｰﾌﾟLOT NO.②
+     */
+    public static final String TOPTAPE_LOTNO2 = "sr_taping_sagyo_toptape_lotno2";
+
+    /**
+     * ﾄｯﾌﾟﾃｰﾌﾟLOT NO.③
+     */
+    public static final String TOPTAPE_LOTNO3 = "sr_taping_sagyo_toptape_lotno3";
+
+    /**
+     * ﾄｯﾌﾟﾃｰﾌﾟLOT NO.④
+     */
+    public static final String TOPTAPE_LOTNO4 = "sr_taping_sagyo_toptape_lotno4";
+
+    /**
+     * ﾄｯﾌﾟﾃｰﾌﾟLOT NO.⑤
+     */
+    public static final String TOPTAPE_LOTNO5 = "sr_taping_sagyo_toptape_lotno5";
+
+    /**
+     * ﾄｯﾌﾟﾃｰﾌﾟLOT NO.⑥
+     */
+    public static final String TOPTAPE_LOTNO6 = "sr_taping_sagyo_toptape_lotno6";
+
+    /**
+     * ﾄｯﾌﾟﾃｰﾌﾟLOT NO.⑦
+     */
+    public static final String TOPTAPE_LOTNO7 = "sr_taping_sagyo_toptape_lotno7";
+
+    /**
+     * ﾄｯﾌﾟﾃｰﾌﾟLOT NO.⑧
+     */
+    public static final String TOPTAPE_LOTNO8 = "sr_taping_sagyo_toptape_lotno8";
+
+    /**
+     * ﾄｯﾌﾟﾃｰﾌﾟLOT NO.⑨
+     */
+    public static final String TOPTAPE_LOTNO9 = "sr_taping_sagyo_toptape_lotno9";
+
+    /**
+     * ﾄｯﾌﾟﾃｰﾌﾟLOT NO.⑩
+     */
+    public static final String TOPTAPE_LOTNO10 = "sr_taping_sagyo_toptape_lotno10";
+
+    /**
+     * ﾎﾞﾄﾑﾃｰﾌﾟLOT NO.①
+     */
+    public static final String BOTTOMTAPE_LOTNO1 = "sr_taping_sagyo_bottomtape_lotno1";
+
+    /**
+     * ﾎﾞﾄﾑﾃｰﾌﾟLOT NO.②
+     */
+    public static final String BOTTOMTAPE_LOTNO2 = "sr_taping_sagyo_bottomtape_lotno2";
+
+    /**
+     * ﾎﾞﾄﾑﾃｰﾌﾟLOT NO.③
+     */
+    public static final String BOTTOMTAPE_LOTNO3 = "sr_taping_sagyo_bottomtape_lotno3";
+
+    /**
+     * 容量範囲ｾｯﾄ値　Hi
+     */
+    public static final String YORYOHANNI_SETCHI_HI = "sr_taping_sagyo_yoryohanni_setchi_hi";
+
+    /**
+     * 容量範囲ｾｯﾄ値　Lo
+     */
+    public static final String YORYOHANNI_SETCHI_LO = "sr_taping_sagyo_yoryohanni_setchi_lo";
+
+    /**
+     * 容量範囲ｾｯﾄ値単位
+     */
+    public static final String YORYOHANNI_SETCHI_TANI = "sr_taping_sagyo_yoryohanni_setchi_tani";
+
+    /**
+     * ﾚﾝｼﾞ表示
+     */
+    public static final String RANGE_HYOJI = "sr_taping_sagyo_range_hyoji";
+
+    /**
+     * ﾚﾝｼﾞ表示単位
+     */
+    public static final String RANGE_HYOJI_TANI = "sr_taping_sagyo_range_hyoji_tani";
+
+    /**
+     * 容量値
+     */
+    public static final String YORYOCHI = "sr_taping_sagyo_yoryochi";
+
+    /**
+     * 容量値単位
+     */
+    public static final String YORYOCHI_TANI = "sr_taping_sagyo_yoryochi_tani";
+
+    /**
+     * DFｾｯﾄ値　Hi
+     */
+    public static final String DF_SETCHI_HI = "sr_taping_sagyo_df_setchi_hi";
+
+    /**
+     * DFｾｯﾄ値　Lo
+     */
+    public static final String DF_SETCHI_LO = "sr_taping_sagyo_df_setchi_lo";
+
+    /**
+     * DF値
+     */
+    public static final String DF_CHI = "sr_taping_sagyo_df_chi";
+
+    /**
+     * 画像設定条件
+     */
+    public static final String GAZO_SETTEIJOKEN = "sr_taping_sagyo_gazo_setteijoken";
+
+    /**
+     * ﾎｯﾊﾟｰﾈｼﾞ確認
+     */
+    public static final String HOPPERNEJI_KAKUNIN = "sr_taping_sagyo_hopperneji_kakunin";
+
+    /**
+     * ｺﾃ清掃
+     */
+    public static final String KOTE_SEISOU = "sr_taping_sagyo_kote_seisou";
+
+    /**
+     * 開始前に製品残なき事
+     */
+    public static final String KAISIMAESEHINZANNASI = "sr_taping_sagyo_kaisimaesehinzannasi";
+
+    /**
+     * SET者
+     */
+    public static final String SETSYA = "sr_taping_sagyo_setsya";
+
+    /**
+     * Wﾁｪｯｸ者
+     */
+    public static final String W_CHECKSYA = "sr_taping_sagyo_w_checksya";
+
+    /**
+     * 開始日
+     */
+    public static final String KAISHI_DAY = "sr_taping_sagyo_kaishi_day";
+
+    /**
+     * 開始時間
+     */
+    public static final String KAISHI_TIME = "sr_taping_sagyo_kaishi_time";
+
+    /**
+     * 試験担当者
+     */
+    public static final String SHIKEN_TANTOSYA = "sr_taping_sagyo_shiken_tantosya";
+
+    /**
+     * 自重落下試験
+     */
+    public static final String JIJU_RAKKA = "sr_taping_sagyo_jiju_rakka";
+
+    /**
+     * ﾊﾞﾗｼﾁｪｯｸ
+     */
+    public static final String BARASHI_CHECK = "sr_taping_sagyo_barashi_check";
+
+    /**
+     * ﾄｯﾌﾟﾃｰﾌﾟ確認
+     */
+    public static final String TOPTAPE_KAKUNIN = "sr_taping_sagyo_toptape_kakunin";
+
+    /**
+     * 挿入数
+     */
+    public static final String SONYUSU = "sr_taping_sagyo_sonyusu";
+
+    /**
+     * 投入数
+     */
+    public static final String TOUNYUSU = "sr_taping_sagyo_tounyusu";
+
+    /**
+     * 良品TPﾘｰﾙ巻数①
+     */
+    public static final String RYOHIN_TP_REEL_MAKISU1 = "sr_taping_sagyo_ryohin_tp_reel_makisu1";
+
+    /**
+     * 良品TPﾘｰﾙ本数①
+     */
+    public static final String RYOHIN_TP_REEL_HONSU1 = "sr_taping_sagyo_ryohin_tp_reel_honsu1";
+
+    /**
+     * 良品TPﾘｰﾙ巻数②
+     */
+    public static final String RYOHIN_TP_REEL_MAKISU2 = "sr_taping_sagyo_ryohin_tp_reel_makisu2";
+
+    /**
+     * 良品TPﾘｰﾙ本数②
+     */
+    public static final String RYOHIN_TP_REEL_HONSU2 = "sr_taping_sagyo_ryohin_tp_reel_honsu2";
+
+    /**
+     * 良品数
+     */
+    public static final String RYOHINSU = "sr_taping_sagyo_ryohinsu";
+
+    /**
+     * 容量NG(NG1)
+     */
+    public static final String YORYO_NG_NG1 = "sr_taping_sagyo_yoryo_ng_ng1";
+
+    /**
+     * 画像NG(上画像数):NG2
+     */
+    public static final String GAZO_NG_UE_GAZOSU_NG2 = "sr_taping_sagyo_gazo_ng_ue_gazosu_ng2";
+
+    /**
+     * 画像NG(下画像数):NG2
+     */
+    public static final String GAZO_NG_SHITA_GAZOSU_NG2 = "sr_taping_sagyo_gazo_ng_shita_gazosu_ng2";
+
+    /**
+     * 歩留まり
+     */
+    public static final String BUDOMARI = "sr_taping_sagyo_budomari";
+
+    /**
+     * 終了日
+     */
+    public static final String SHURYOU_DAY = "sr_taping_sagyo_shuryou_day";
+
+    /**
+     * 終了時間
+     */
+    public static final String SHURYOU_TIME = "sr_taping_sagyo_shuryou_time";
+
+    /**
+     * ﾒﾝﾃﾅﾝｽ回数
+     */
+    public static final String MAINTENANCE_KAISU = "sr_taping_sagyo_maintenance_kaisu";
+
+    /**
+     * ﾒﾝﾃ後TP外観
+     */
+    public static final String MAINTGO_TP_GAIKAN = "sr_taping_sagyo_maintgo_tp_gaikan";
+
+    /**
+     * ﾊﾞﾗｼ対象ﾘｰﾙ数
+     */
+    public static final String BARASHI_TAISYO_REELSU = "sr_taping_sagyo_barashi_taisyo_reelsu";
+
+    /**
+     * 空ﾘｰﾙ数
+     */
+    public static final String KARA_REELSU = "sr_taping_sagyo_kara_reelsu";
+
+    /**
+     * 良品ﾘｰﾙ数
+     */
+    public static final String RYOHIN_REELSU = "sr_taping_sagyo_ryohin_reelsu";
+
+    /**
+     * QA確認依頼ﾘｰﾙ数
+     */
+    public static final String QAKAKUNIN_IRAI_REELSU = "sr_taping_sagyo_qakakunin_irai_reelsu";
+
+    /**
+     * ﾘｰﾙ数ﾁｪｯｸ
+     */
+    public static final String REELSU_CHECK = "sr_taping_sagyo_reelsu_check";
+
+    /**
+     * TP後清掃：ﾎｯﾊﾟｰ部
+     */
+    public static final String TPATO_SEISOU_HOPPER_PART = "sr_taping_sagyo_tpato_seisou_hopper_part";
+
+    /**
+     * TP後清掃：ﾌｨｰﾀﾞ部
+     */
+    public static final String TPATO_SEISOU_FEEDER_PART = "sr_taping_sagyo_tpato_seisou_feeder_part";
+
+    /**
+     * TP後清掃：INDEX内
+     */
+    public static final String TPATO_SEISOU_IN_INDEX = "sr_taping_sagyo_tpato_seisou_in_index";
+
+    /**
+     * TP後清掃：NGBOX内
+     */
+    public static final String TPATO_SEISOU_IN_NGBOX = "sr_taping_sagyo_tpato_seisou_in_ngbox";
+
+    /**
+     * 清掃担当者
+     */
+    public static final String SEISOU_TANTOUSYA = "sr_taping_sagyo_seisou_tantousya";
+
+    /**
+     * 清掃確認者
+     */
+    public static final String SEISOU_KAKUNINSYA = "sr_taping_sagyo_seisou_kakuninsya";
+
+    /**
+     * ﾊﾞﾗｼ依頼ﾘｰﾙ数
+     */
+    public static final String BARASHI_IRAI_REELSU = "sr_taping_sagyo_barashi_irai_reelsu";
+
+    /**
+     * ﾊﾞﾗｼ開始日
+     */
+    public static final String BARASHI_KAISHI_DAY = "sr_taping_sagyo_barashi_kaishi_day";
+
+    /**
+     * ﾊﾞﾗｼ開始時間
+     */
+    public static final String BARASHI_KAISHI_TIME = "sr_taping_sagyo_barashi_kaishi_time";
+
+    /**
+     * ﾊﾞﾗｼ終了日
+     */
+    public static final String BARASHI_SHURYOU_DAY = "sr_taping_sagyo_barashi_shuryou_day";
+
+    /**
+     * ﾊﾞﾗｼ終了時間
+     */
+    public static final String BARASHI_SHURYOU_TIME = "sr_taping_sagyo_barashi_shuryou_time";
+
+    /**
+     * ﾊﾞﾗｼ担当者
+     */
+    public static final String BARASHI_TANTOUSYA = "sr_taping_sagyo_barashi_tantousya";
+
+    /**
+     * 脱磁依頼ﾘｰﾙ数
+     */
+    public static final String DATSUJI_IRAI_REELSU = "sr_taping_sagyo_datsuji_irai_reelsu";
+
+    /**
+     * 脱磁開始日
+     */
+    public static final String DATSUJI_KAISHI_DAY = "sr_taping_sagyo_datsuji_kaishi_day";
+
+    /**
+     * 脱磁開始時間
+     */
+    public static final String DATSUJI_KAISHI_TIME = "sr_taping_sagyo_datsuji_kaishi_time";
+
+    /**
+     * 脱磁担当者
+     */
+    public static final String DATSUJI_TANTOUSYA = "sr_taping_sagyo_datsuji_tantousya";
+
+    /**
+     * 確保ﾘｰﾙ数
+     */
+    public static final String KAKUHO_REELSU = "sr_taping_sagyo_kakuho_reelsu";
+
+    /**
+     * 空ﾘｰﾙ数2
+     */
+    public static final String KARA_REELSU2 = "sr_taping_sagyo_kara_reelsu2";
+
+    /**
+     * 良品ﾘｰﾙ数2
+     */
+    public static final String RYOHIN_REELSU2 = "sr_taping_sagyo_ryohin_reelsu2";
+
+    /**
+     * QA確認依頼ﾘｰﾙ数2
+     */
+    public static final String QAKAKUNIN_IRAI_REELSU2 = "sr_taping_sagyo_qakakunin_irai_reelsu2";
+
+    /**
+     * 最終確認担当者
+     */
+    public static final String SAISYUKAKUNIN_TANTOUSYA = "sr_taping_sagyo_saisyukakunin_tantousya";
+
+    /**
+     * 備考1
+     */
+    public static final String BIKOU1 = "sr_taping_sagyo_bikou1";
+
+    /**
+     * 備考2
+     */
+    public static final String BIKOU2 = "sr_taping_sagyo_bikou2";
+
+    /**
+     * 枝番ｺﾋﾟｰ(画面上ボタン)
+     */
+    public static final String BTN_COPY_EDABAN_TOP = "sr_taping_sagyo_copy_edaban_Top";
+
+    /**
+     * 仮登録(画面上ボタン)
+     */
+    public static final String BTN_KARI_TOUROKU_TOP = "sr_taping_sagyo_kari_touroku_Top";
+
+    /**
+     * 登録(画面上ボタン)
+     */
+    public static final String BTN_INSERT_TOP = "sr_taping_sagyo_insert_Top";
+
+    /**
+     * 修正(画面上ボタン)
+     */
+    public static final String BTN_UPDATE_TOP = "sr_taping_sagyo_update_Top";
+
+    /**
+     * 削除(画面上ボタン)
+     */
+    public static final String BTN_DELETE_TOP = "sr_taping_sagyo_delete_Top";
+
+    /**
+     * 作業開始(画面上ボタン)
+     */
+    public static final String BTN_SAGYO_START_TOP = "sr_taping_sagyo_sagyo_start_Top";
+
+    /**
+     * 作業終了(画面上ボタン)
+     */
+    public static final String BTN_SAGYO_END_TOP = "sr_taping_sagyo_sagyo_end_Top";
+
+    /**
+     * ﾊﾞﾗｼ開始(画面上ボタン)
+     */
+    public static final String BTN_BARASHI_START_TOP = "sr_taping_sagyo_barashi_start_Top";
+
+    /**
+     * ﾊﾞﾗｼ終了(画面上ボタン)
+     */
+    public static final String BTN_BARASHI_END_TOP = "sr_taping_sagyo_barashi_end_Top";
+
+    /**
+     * 脱磁開始(画面上ボタン)
+     */
+    public static final String BTN_DATSUJI_START_TOP = "sr_taping_sagyo_datsuji_start_Top";
+
+    /**
+     * TP作業注意(画面上ボタン)
+     */
+    public static final String BTN_TP_SAGYO_TYUI_TOP = "sr_taping_sagyo_tp_sagyo_tyui_Top";
+    /**
+     * ﾊﾞﾗｼ作業注意(画面上ボタン)
+     */
+    public static final String BTN_BARASHI_SAGYO_TYUI_TOP = "sr_taping_sagyo_barashi_sagyo_tyui_Top";
+    /**
+     * ﾘｰﾙ計数注意(画面上ボタン)
+     */
+    public static final String BTN_REELKEISU_TYUI_TOP = "sr_taping_sagyo_reelkeisu_tyui_Top";
+
+    /**
+     * 確保数計算(画面上ボタン)
+     */
+    public static final String BTN_KAKUHOSU_KEISAN_TOP = "sr_taping_sagyo_kakuhosu_keisan_Top";
+
+    /**
+     * 良品数計算(画面上ボタン)
+     */
+    public static final String BTN_RYOHINSU_KEISAN_TOP = "sr_taping_sagyo_ryohinsu_keisan_Top";
+
+    /**
+     * 歩留まり計算(画面上ボタン)
+     */
+    public static final String BTN_BUDOMARI_KEISAN_TOP = "sr_taping_sagyo_budomari_keisan_Top";
+
+    /**
+     * 受入れ総重量計算(画面上ボタン)
+     */
+    public static final String BTN_UKEIRE_SOUJURYO_KEISAN_TOP = "sr_taping_sagyo_ukeire_soujuryo_keisan_Top";
+
+    /**
+     * 枝番ｺﾋﾟｰ(画面下ボタン)
+     */
+    public static final String BTN_COPY_EDABAN_BOTTOM = "sr_taping_sagyo_copy_edaban_Bottom";
+
+    /**
+     * 仮登録(画面下ボタン)
+     */
+    public static final String BTN_KARI_TOUROKU_BOTTOM = "sr_taping_sagyo_kari_touroku_Bottom";
+
+    /**
+     * 登録(画面下ボタン)
+     */
+    public static final String BTN_INSERT_BOTTOM = "sr_taping_sagyo_insert_Bottom";
+
+    /**
+     * 修正(画面下ボタン)
+     */
+    public static final String BTN_UPDATE_BOTTOM = "sr_taping_sagyo_update_Bottom";
+
+    /**
+     * 削除(画面下ボタン)
+     */
+    public static final String BTN_DELETE_BOTTOM = "sr_taping_sagyo_delete_Bottom";
+
+    /**
+     * 作業開始(画面下ボタン)
+     */
+    public static final String BTN_SAGYO_START_BOTTOM = "sr_taping_sagyo_sagyo_start_Bottom";
+
+    /**
+     * 作業終了(画面下ボタン)
+     */
+    public static final String BTN_SAGYO_END_BOTTOM = "sr_taping_sagyo_sagyo_end_Bottom";
+
+    /**
+     * ﾊﾞﾗｼ開始(画面下ボタン)
+     */
+    public static final String BTN_BARASHI_START_BOTTOM = "sr_taping_sagyo_barashi_start_Bottom";
+
+    /**
+     * ﾊﾞﾗｼ終了(画面下ボタン)
+     */
+    public static final String BTN_BARASHI_END_BOTTOM = "sr_taping_sagyo_barashi_end_Bottom";
+
+    /**
+     * 脱磁開始(画面下ボタン)
+     */
+    public static final String BTN_DATSUJI_START_BOTTOM = "sr_taping_sagyo_datsuji_start_Bottom";
+
+    /**
+     * TP作業注意(画面下ボタン)
+     */
+    public static final String BTN_TP_SAGYO_TYUI_BOTTOM = "sr_taping_sagyo_tp_sagyo_tyui_Bottom";
+    /**
+     * ﾊﾞﾗｼ作業注意(画面下ボタン)
+     */
+    public static final String BTN_BARASHI_SAGYO_TYUI_BOTTOM = "sr_taping_sagyo_barashi_sagyo_tyui_Bottom";
+    /**
+     * ﾘｰﾙ計数注意(画面下ボタン)
+     */
+    public static final String BTN_REELKEISU_TYUI_BOTTOM = "sr_taping_sagyo_reelkeisu_tyui_Bottom";
+
+    /**
+     * 確保数計算(画面下ボタン)
+     */
+    public static final String BTN_KAKUHOSU_KEISAN_BOTTOM = "sr_taping_sagyo_kakuhosu_keisan_Bottom";
+
+    /**
+     * 良品数計算(画面下ボタン)
+     */
+    public static final String BTN_RYOHINSU_KEISAN_BOTTOM = "sr_taping_sagyo_ryohinsu_keisan_Bottom";
+
+    /**
+     * 歩留まり計算(画面下ボタン)
+     */
+    public static final String BTN_BUDOMARI_KEISAN_BOTTOM = "sr_taping_sagyo_budomari_keisan_Bottom";
+
+    /**
+     * 受入れ総重量計算(画面下ボタン)
+     */
+    public static final String BTN_UKEIRE_SOUJURYO_KEISAN_BOTTOM = "sr_taping_sagyo_ukeire_soujuryo_keisan_Bottom";
+
+    /**
+     * ユーザー認証パラメータ(修正)
+     */
+    public static final String USER_AUTH_UPDATE_PARAM = "sr_taping_sagyo_update_button";
+
+    /**
+     * ユーザー認証パラメータ(削除)
+     */
+    public static final String USER_AUTH_DELETE_PARAM = "sr_taping_sagyo_delete_button";
+
+}
