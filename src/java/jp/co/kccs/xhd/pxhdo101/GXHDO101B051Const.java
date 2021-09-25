bdc282627ee5c021092d2aa85f408d1cd7d8ef53
Yusuke Kurozumi <srckurozumi@gmail.com>
2021-09-25 09:46:49 +0900

    GXHDO101B51：新規作成(ビルド未完)

---
+++ src/java/jp/co/kccs/xhd/pxhdo101/GXHDO101B051Const.java
@@ -0,0 +1,200 @@
+/*
+ * Copyright 2019 Kyocera Communication Systems Co., Ltd All rights reserved.
+ */
+package jp.co.kccs.xhd.pxhdo101;
+
+/**
+ * ===============================================================================<br>
+ * <br>
+ * システム名	品質DB(コンデンサ)<br>
+ * <br>
+ * 変更日	2021/09/22<br>
+ * 計画書No	MB2108-DK001<br>
+ * 変更者	SRC H.Yamagata<br>
+ * 変更理由	新規作成<br>
+ * <br>
+ * ===============================================================================<br>
+ */
+/**
+ * GXHDO101B020Const(焼成・真空脱脂)定数
+ *
+ * @author SRC H.Yamagata
+ * @since  2021/09/22
+ */
+public class GXHDO101B051Const {
+    /**
+     * 工場ｺｰﾄﾞ
+    */
+    public static final String UDASSI_KOJYO = "sr_shinkuudassi_kojyo";
+    /**
+     * ﾛｯﾄNo.
+    */
+    public static final String UDASSI_LOTNO = "sr_shinkuudassi_lotno";
+    /**
+     * KCPNO
+    */
+    public static final String UDASSI_KCPNO = "sr_shinkuudassi_kcpno";
+    /**
+     * 客先
+    */
+    public static final String UDASSI_KYAKUSAKI = "sr_shinkuudassi_kyakusaki";
+    /**
+     * ﾛｯﾄ区分
+    */
+    public static final String UDASSI_LOT_KUBUN = "sr_shinkuudassi_lot_kubun";
+    /**
+     * ｵｰﾅｰ
+    */
+    public static final String UDASSI_OWNER = "sr_shinkuudassi_owner";
+    /**
+     * 受入ｻﾔ枚数
+    */
+    public static final String UDASSI_UKEIRE_SAYA_MAISU = "sr_shinkuudassi_ukeire_saya_maisu";
+    /**
+     * 号機
+    */
+    public static final String UDASSI_GOKI = "sr_shinkuudassi_goki";
+    /**
+     * ﾌﾟﾛｸﾞﾗﾑNo．
+    */
+    public static final String UDASSI_PROGRAM_NO = "sr_shinkuudassi_program_no";
+    /**
+     * 最高温度
+    */
+    public static final String UDASSI_MAX_ONDO = "sr_shinkuudassi_max_ondo";
+    /**
+     * ｷｰﾌﾟ時間
+    */
+    public static final String UDASSI_KEEP_TIME = "sr_shinkuudassi_keep_time";
+    /**
+     * 総時間
+    */
+    public static final String UDASSI_TOTAL_TIME = "sr_shinkuudassi_total_time";
+    /**
+     * 投入ｻﾔ枚数
+    */
+    public static final String UDASSI_TOUNYU_SAYA_MAISU = "sr_shinkuudassi_tounyu_saya_maisu";
+    /**
+     * 開始日
+    */
+    public static final String UDASSI_KAISHI_DAY = "sr_shinkuudassi_kaishi_day";
+    /**
+     * 開始時間
+    */
+    public static final String UDASSI_KAISHI_TIME = "sr_shinkuudassi_kaishi_time";
+    /**
+     * 開始担当者
+    */
+    public static final String UDASSI_KAISHI_TANTOUSYA = "sr_shinkuudassi_kaishi_tantousya";
+    /**
+     * 開始確認者
+    */
+    public static final String UDASSI_KAKUNINSYA = "sr_shinkuudassi_kakuninsya";
+    /**
+     * 終了日
+    */
+    public static final String UDASSI_SHURYOU_DAY = "sr_shinkuudassi_shuryou_day";
+    /**
+     * 終了時間
+    */
+    public static final String UDASSI_SHURYOU_TIME = "sr_shinkuudassi_shuryou_time";
+    /**
+     * 終了担当者
+    */
+    public static final String UDASSI_SHURYOU_TANTOUSYA = "sr_shinkuudassi_shuryou_tantousya";
+    /**
+     * 回収ｻﾔ枚数
+    */
+    public static final String UDASSI_KAISHU_SAYA_MAISU = "sr_shinkuudassi_kaishu_saya_maisu";
+    /**
+     * 備考1
+    */
+    public static final String UDASSI_BIKO1 = "sr_shinkuudassi_biko1";
+    /**
+     * 備考2
+    */
+    public static final String UDASSI_BIKO2 = "sr_shinkuudassi_biko2";  
+    /**
+     * 備考3
+    */
+    public static final String UDASSI_BIKO3 = "sr_shinkuudassi_biko3";
+    /**
+     * 備考4
+    */
+    public static final String UDASSI_BIKO4 = "sr_shinkuudassi_biko4";  
+    /**
+     * 枝番ｺﾋﾟｰ(画面上ボタン)
+    */
+    public static final String BTN_I_COPY_EDABAN_TOP = "sr_shinkuudassi_copy_edaban_Top";
+    /**
+     * 仮登録(画面上ボタン)
+    */
+    public static final String BTN_I_KARI_TOUROKU_TOP = "sr_shinkuudassi_kari_touroku_Top";
+    /**
+     * 登録(画面上ボタン)
+    */
+    public static final String BTN_I_INSERT_TOP = "sr_shinkuudassi_insert_Top";
+    /**
+     * 修正(画面上ボタン)
+    */
+    public static final String BTN_I_UPDATE_TOP = "sr_shinkuudassi_update_Top";
+    /**
+     * 削除(画面上ボタン)
+    */
+    public static final String BTN_I_DELETE_TOP = "sr_shinkuudassi_delete_Top";
+    /**
+     * 開始日時(画面上ボタン)
+    */
+    public static final String BTN_I_STARTDATETIME_TOP = "sr_shinkuudassi_startdatetime_Top";
+    /**
+     * 終了日時(画面上ボタン)
+    */
+    public static final String BTN_I_ENDDATETIME_TOP = "sr_shinkuudassi_enddatetime_Top";
+    /**
+     * ﾌﾛｰ用ﾎﾞﾀﾝ(画面上ボタン)
+    */
+    public static final String BTN_I_FLOW_BUTTON_TOP = "sr_shinkuudassi_flow_button_Top";
+    /**
+     * 枝番ｺﾋﾟｰ(画面下ボタン)
+    */
+    public static final String BTN_I_COPY_EDABAN_BOTTOM = "sr_shinkuudassi_copy_edaban_Bottom";
+    /**
+     * 仮登録(画面下ボタン)
+    */
+    public static final String BTN_I_KARI_TOUROKU_BOTTOM = "sr_shinkuudassi_kari_touroku_Bottom";
+    /**
+     * 登録(画面下ボタン)
+    */
+    public static final String BTN_I_INSERT_BOTTOM = "sr_shinkuudassi_insert_Bottom";
+    /**
+     * 修正(画面下ボタン)
+    */
+    public static final String BTN_I_UPDATE_BOTTOM = "sr_shinkuudassi_update_Bottom";
+    /**
+     * 削除(画面下ボタン)
+    */
+    public static final String BTN_I_DELETE_BOTTOM = "sr_shinkuudassi_delete_Bottom";
+    /**
+     * 開始日時(画面下ボタン)
+    */
+    public static final String BTN_I_STARTDATETIME_BOTTOM = "sr_shinkuudassi_startdatetime_Bottom";
+    /**
+     * 終了日時(画面下ボタン)
+    */
+    public static final String BTN_I_ENDDATETIME_BOTTOM = "sr_shinkuudassi_enddatetime_Bottom";
+    /**
+     * ﾌﾛｰ用ﾎﾞﾀﾝ(画面下ボタン)
+    */
+    public static final String BTN_I_FLOW_BUTTON_BOTTOM = "sr_shinkuudassi_flow_button_Bottom";
+
+    /**
+     * ユーザー認証パラメータ(修正)
+     */
+    public static final String USER_AUTH_UPDATE_PARAM = "sr_shinkuudassi_update_button";
+
+    /**
+     * ユーザー認証パラメータ(削除)
+     */
+    public static final String USER_AUTH_DELETE_PARAM = "sr_shinkuudassi_delete_button";
+
+}
