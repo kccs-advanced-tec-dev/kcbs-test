bd4287d2d98917475f87a06a94e39a3a4bb332c5
miki2-takahashi <miki2-takahashi@kccs.co.jp>
2021-11-05 09:34:26 +0900

    電子化　添加材ｶﾞﾗｽ　新規作成

---
+++ src/java/jp/co/kccs/xhd/pxhdo102/GXHDO102B007Const.java
@@ -0,0 +1,575 @@
+/*
+ * Copyright 2021 Kyocera Communication Systems Co., Ltd All rights reserved.
+ */
+package jp.co.kccs.xhd.pxhdo102;
+
+/**
+ * ===============================================================================<br>
+ * <br>
+ * システム名	品質DB(コンデンサ)<br>
+ * <br>
+ * 変更日	2021/10/08<br>
+ * 計画書No	MB2101-DK002<br>
+ * 変更者	KCSS K.Jo<br>
+ * 変更理由	新規作成<br>
+ * <br>
+ * ===============================================================================<br>
+ */
+/**
+ * GXHDO102B007(添加材ｽﾗﾘｰ作製・ｶﾞﾗｽ)
+ *
+ * @author KCSS K.Jo
+ * @since 2021/10/08
+ */
+public class GXHDO102B007Const {
+
+    /**
+     * WIPﾛｯﾄNo
+     */
+    public static final String WIPLOTNO = "sr_tenka_glass_wiplotno";
+
+    /**
+     * 添加材ｽﾗﾘｰ品名
+     */
+    public static final String TENKAHINMEI = "sr_tenka_glass_tenkahinmei";
+
+    /**
+     * 添加材ｽﾗﾘｰLotNo
+     */
+    public static final String TENKALOTNO = "sr_tenka_glass_tenkalotno";
+
+    /**
+     * ﾛｯﾄ区分
+     */
+    public static final String LOTKUBUN = "sr_tenka_glass_lotkubun";
+
+    /**
+     * 固形分測定日
+     */
+    public static final String KOKEIBUNSOKUTEI_DAY = "sr_tenka_glass_kokeibunsokutei_day";
+
+    /**
+     * 固形分測定時間
+     */
+    public static final String KOKEIBUNSOKUTEI_TIME = "sr_tenka_glass_kokeibunsokutei_time";
+
+    /**
+     * 容器重量
+     */
+    public static final String YOUKIJYUURYOU = "sr_tenka_glass_youkijyuuryou";
+
+    /**
+     * 総重量
+     */
+    public static final String SOUJYUURYOU = "sr_tenka_glass_soujyuuryou";
+
+    /**
+     * 正味重量
+     */
+    public static final String SYOUMIJYUURYOU = "sr_tenka_glass_syoumijyuuryou";
+
+    /**
+     * 撹拌機
+     */
+    public static final String KAKUHANKI = "sr_tenka_glass_kakuhanki";
+
+    /**
+     * 撹拌時間
+     */
+    public static final String KAKUHAJIKAN = "sr_tenka_glass_kakuhajikan";
+
+    /**
+     * 撹拌開始日
+     */
+    public static final String KAKUHANKAISI_DAY = "sr_tenka_glass_kakuhankaisi_day";
+
+    /**
+     * 撹拌開始時間
+     */
+    public static final String KAKUHANKAISI_TIME = "sr_tenka_glass_kakuhankaisi_time";
+
+    /**
+     * 撹拌終了日
+     */
+    public static final String KAKUHANSYUURYOU_DAY = "sr_tenka_glass_kakuhansyuuryou_day";
+
+    /**
+     * 撹拌終了時間
+     */
+    public static final String KAKUHANSYUURYOU_TIME = "sr_tenka_glass_kakuhansyuuryou_time";
+
+    /**
+     * 撹拌担当者
+     */
+    public static final String KAKUHANTANTOUSYA = "sr_tenka_glass_kakuhantantousya";
+
+    /**
+     * ﾙﾂﾎﾞの種類
+     */
+    public static final String RUTUBO = "sr_tenka_glass_rutubo";
+
+    /**
+     * 風袋重量
+     */
+    public static final String FUUTAIJYUURYOU = "sr_tenka_glass_fuutaijyuuryou";
+
+    /**
+     * 乾燥前重量
+     */
+    public static final String KANSOUMAEJYUURYOU = "sr_tenka_glass_kansoumaejyuuryou";
+
+    /**
+     * 乾燥方法①
+     */
+    public static final String KANSOUHOUHOU1 = "sr_tenka_glass_kansouhouhou1";
+
+    /**
+     * 乾燥温度①
+     */
+    public static final String KANSOUONDO1 = "sr_tenka_glass_kansouondo1";
+
+    /**
+     * 乾燥時間①
+     */
+    public static final String KANSOUJIKAN1 = "sr_tenka_glass_kansoujikan1";
+
+    /**
+     * 乾燥開始日①
+     */
+    public static final String KANSOUKAISI1_DAY = "sr_tenka_glass_kansoukaisi1_day";
+
+    /**
+     * 乾燥開始時間①
+     */
+    public static final String KANSOUKAISI1_TIME = "sr_tenka_glass_kansoukaisi1_time";
+
+    /**
+     * 乾燥終了日①
+     */
+    public static final String KANSOUSYUURYOU1_DAY = "sr_tenka_glass_kansousyuuryou1_day";
+
+    /**
+     * 乾燥終了時間①
+     */
+    public static final String KANSOUSYUURYOU1_TIME = "sr_tenka_glass_kansousyuuryou1_time";
+
+    /**
+     * 乾燥方法②
+     */
+    public static final String KANSOUHOUHOU2 = "sr_tenka_glass_kansouhouhou2";
+
+    /**
+     * 乾燥温度②
+     */
+    public static final String KANSOUONDO2 = "sr_tenka_glass_kansouondo2";
+
+    /**
+     * 乾燥時間②
+     */
+    public static final String KANSOUJIKAN2 = "sr_tenka_glass_kansoujikan2";
+
+    /**
+     * 乾燥開始日②
+     */
+    public static final String KANSOUKAISI2_DAY = "sr_tenka_glass_kansoukaisi2_day";
+
+    /**
+     * 乾燥開始時間②
+     */
+    public static final String KANSOUKAISI2_TIME = "sr_tenka_glass_kansoukaisi2_time";
+
+    /**
+     * 乾燥終了日②
+     */
+    public static final String KANSOUSYUURYOU2_DAY = "sr_tenka_glass_kansousyuuryou2_day";
+
+    /**
+     * 乾燥終了時間②
+     */
+    public static final String KANSOUSYUURYOU2_TIME = "sr_tenka_glass_kansousyuuryou2_time";
+
+    /**
+     * 乾燥後総重量
+     */
+    public static final String KANSOUGOSOUJYUURYOU = "sr_tenka_glass_kansougosoujyuuryou";
+
+    /**
+     * 乾燥後重量
+     */
+    public static final String KANSOUGOJYUURYOU = "sr_tenka_glass_kansougojyuuryou";
+
+    /**
+     * 固形分
+     */
+    public static final String KOKEIBUN = "sr_tenka_glass_kokeibun";
+
+    /**
+     * 固形分測定担当者
+     */
+    public static final String KOKEIBUNSOKUTEITANTOUSYA = "sr_tenka_glass_kokeibunsokuteitantousya";
+
+    /**
+     * 固形分補正量
+     */
+    public static final String KOKEIBUNHOSEIRYOU = "sr_tenka_glass_kokeibunhoseiryou";
+
+    /**
+     * 溶剤①添加量
+     */
+    public static final String YOUZAI1TENKARYOU = "sr_tenka_glass_youzai1tenkaryou";
+
+    /**
+     * 溶剤①調合量
+     */
+    public static final String YOUZAI1TYOUGOURYOU = "sr_tenka_glass_youzai1tyougouryou";
+
+    /**
+     * 溶剤②添加量
+     */
+    public static final String YOUZAI2TENKARYOU = "sr_tenka_glass_youzai2tenkaryou";
+
+    /**
+     * 溶剤②調合量
+     */
+    public static final String YOUZAI2TYOUGOURYOU = "sr_tenka_glass_youzai2tyougouryou";
+
+    /**
+     * 2回目撹拌機
+     */
+    public static final String KAKUHANKI2 = "sr_tenka_glass_kakuhanki2";
+
+    /**
+     * 2回目撹拌時間
+     */
+    public static final String KAKUHAJIKAN2 = "sr_tenka_glass_kakuhajikan2";
+
+    /**
+     * 2回目撹拌開始日
+     */
+    public static final String KAKUHANKAISI2_DAY = "sr_tenka_glass_kakuhankaisi2_day";
+
+    /**
+     * 2回目撹拌開始時間
+     */
+    public static final String KAKUHANKAISI2_TIME = "sr_tenka_glass_kakuhankaisi2_time";
+
+    /**
+     * 2回目撹拌終了日
+     */
+    public static final String KAKUHANSYUURYOU2_DAY = "sr_tenka_glass_kakuhansyuuryou2_day";
+
+    /**
+     * 2回目撹拌終了時間
+     */
+    public static final String KAKUHANSYUURYOU2_TIME = "sr_tenka_glass_kakuhansyuuryou2_time";
+
+    /**
+     * 2回目撹拌担当者
+     */
+    public static final String KAKUHANTANTOUSYA2 = "sr_tenka_glass_kakuhantantousya2";
+
+    /**
+     * 2回目ﾙﾂﾎﾞの種類
+     */
+    public static final String RUTUBO2 = "sr_tenka_glass_rutubo2";
+
+    /**
+     * 2回目風袋重量
+     */
+    public static final String FUUTAIJYUURYOU2 = "sr_tenka_glass_fuutaijyuuryou2";
+
+    /**
+     * 2回目乾燥前重量
+     */
+    public static final String KANSOUMAEJYUURYOU2 = "sr_tenka_glass_kansoumaejyuuryou2";
+
+    /**
+     * 2回目乾燥方法①
+     */
+    public static final String KANSOUHOUHOU1_2 = "sr_tenka_glass_kansouhouhou1_2";
+
+    /**
+     * 2回目乾燥温度①
+     */
+    public static final String KANSOUONDO1_2 = "sr_tenka_glass_kansouondo1_2";
+
+    /**
+     * 2回目乾燥時間①
+     */
+    public static final String KANSOUJIKAN1_2 = "sr_tenka_glass_kansoujikan1_2";
+
+    /**
+     * 2回目乾燥開始日①
+     */
+    public static final String KANSOUKAISI1_2_DAY = "sr_tenka_glass_kansoukaisi1_2_day";
+
+    /**
+     * 2回目乾燥開始時間①
+     */
+    public static final String KANSOUKAISI1_2_TIME = "sr_tenka_glass_kansoukaisi1_2_time";
+
+    /**
+     * 2回目乾燥終了日①
+     */
+    public static final String KANSOUSYUURYOU1_2_DAY = "sr_tenka_glass_kansousyuuryou1_2_day";
+
+    /**
+     * 2回目乾燥終了時間①
+     */
+    public static final String KANSOUSYUURYOU1_2_TIME = "sr_tenka_glass_kansousyuuryou1_2_time";
+
+    /**
+     * 2回目乾燥後総重量
+     */
+    public static final String KANSOUGOSOUJYUURYOU2 = "sr_tenka_glass_kansougosoujyuuryou2";
+
+    /**
+     * 2回目乾燥後重量
+     */
+    public static final String KANSOUGOJYUURYOU2 = "sr_tenka_glass_kansougojyuuryou2";
+
+    /**
+     * 2回目固形分
+     */
+    public static final String KOKEIBUN2 = "sr_tenka_glass_kokeibun2";
+
+    /**
+     * 2回目固形分測定担当者
+     */
+    public static final String KOKEIBUNSOKUTEITANTOUSYA2 = "sr_tenka_glass_kokeibunsokuteitantousya2";
+
+    /**
+     * 確認者
+     */
+    public static final String KAKUNINSYA = "sr_tenka_glass_kakuninsya";
+
+    /**
+     * 備考1
+     */
+    public static final String BIKOU1 = "sr_tenka_glass_bikou1";
+
+    /**
+     * 備考2
+     */
+    public static final String BIKOU2 = "sr_tenka_glass_bikou2";
+
+    /**
+     * 枝番ｺﾋﾟｰ(画面上ボタン)
+     */
+    public static final String BTN_EDABAN_COPY_TOP = "sr_tenka_glass_edaban_copy_Top";
+
+    /**
+     * 固形分測定日時(画面上ボタン)
+     */
+    public static final String BTN_KOKEIBUNSOKUTEINICHIJI_TOP = "sr_tenka_glass_kokeibunsokuteinichiji_Top";
+
+    /**
+     * 正味重量計算(画面上ボタン)
+     */
+    public static final String BTN_SYOUMIJYUURYOU_KEISAN_TOP = "sr_tenka_glass_syoumijyuuryou_keisan_Top";
+
+    /**
+     * 撹拌開始日時(画面上ボタン)
+     */
+    public static final String BTN_KAKUHANKAISINICHIJI_TOP = "sr_tenka_glass_kakuhankaisinichiji_Top";
+
+    /**
+     * 撹拌終了日時(画面上ボタン)
+     */
+    public static final String BTN_KAKUHANSYUURYOUNICHIJI_TOP = "sr_tenka_glass_kakuhansyuuryounichiji_Top";
+
+    /**
+     * 乾燥開始日時①(画面上ボタン)
+     */
+    public static final String BTN_KANSOUKAISINICHIJI1_TOP = "sr_tenka_glass_kansoukaisinichiji1_Top";
+
+    /**
+     * 乾燥終了日時①(画面上ボタン)
+     */
+    public static final String BTN_KANSOUSYUURYOUNICHIJI1_TOP = "sr_tenka_glass_kansousyuuryounichiji1_Top";
+
+    /**
+     * 乾燥開始日時②(画面上ボタン)
+     */
+    public static final String BTN_KANSOUKAISINICHIJI2_TOP = "sr_tenka_glass_kansoukaisinichiji2_Top";
+
+    /**
+     * 乾燥終了日時②(画面上ボタン)
+     */
+    public static final String BTN_KANSOUSYUURYOUNICHIJI2_TOP = "sr_tenka_glass_kansousyuuryounichiji2_Top";
+
+    /**
+     * 乾燥後重量計算(画面上ボタン)
+     */
+    public static final String BTN_KANSOUGOJYUURYOU_KEISAN_TOP = "sr_tenka_glass_kansougojyuuryou_keisan_Top";
+
+    /**
+     * 固形分計算(画面上ボタン)
+     */
+    public static final String BTN_KOKEIBUN_KEISAN_TOP = "sr_tenka_glass_kokeibun_keisan_Top";
+
+    /**
+     * 2回目撹拌開始日時(画面上ボタン)
+     */
+    public static final String BTN_KAKUHANKAISINICHIJI2_TOP = "sr_tenka_glass_kakuhankaisinichiji2_Top";
+
+    /**
+     * 2回目撹拌終了日時(画面上ボタン)
+     */
+    public static final String BTN_KAKUHANSYUURYOUNICHIJI2_TOP = "sr_tenka_glass_kakuhansyuuryounichiji2_Top";
+
+    /**
+     * 2回目乾燥開始日時①(画面上ボタン)
+     */
+    public static final String BTN_KANSOUKAISINICHIJI1_2_TOP = "sr_tenka_glass_kansoukaisinichiji1_2_Top";
+
+    /**
+     * 2回目乾燥終了日時①(画面上ボタン)
+     */
+    public static final String BTN_KANSOUSYUURYOUNICHIJI1_2_TOP = "sr_tenka_glass_kansousyuuryounichiji1_2_Top";
+
+    /**
+     * 2回目乾燥後重量計算(画面上ボタン)
+     */
+    public static final String BTN_KANSOUGOJYUURYOU2_KEISAN_TOP = "sr_tenka_glass_kansougojyuuryou2_keisan_Top";
+
+    /**
+     * 2回目固形分計算(画面上ボタン)
+     */
+    public static final String BTN_KOKEIBUN2_KEISAN_TOP = "sr_tenka_glass_kokeibun2_keisan_Top";
+
+    /**
+     * 仮登録(画面上ボタン)
+     */
+    public static final String BTN_KARI_TOUROKU_TOP = "sr_tenka_glass_kari_touroku_Top";
+
+    /**
+     * 登録(画面上ボタン)
+     */
+    public static final String BTN_INSERT_TOP = "sr_tenka_glass_insert_Top";
+
+    /**
+     * 修正(画面上ボタン)
+     */
+    public static final String BTN_UPDATE_TOP = "sr_tenka_glass_update_Top";
+
+    /**
+     * 削除(画面上ボタン)
+     */
+    public static final String BTN_DELETE_TOP = "sr_tenka_glass_delete_Top";
+
+    /**
+     * 枝番ｺﾋﾟｰ(画面下ボタン)
+     */
+    public static final String BTN_EDABAN_COPY_BOTTOM = "sr_tenka_glass_edaban_copy_Bottom";
+
+    /**
+     * 固形分測定日時(画面下ボタン)
+     */
+    public static final String BTN_KOKEIBUNSOKUTEINICHIJI_BOTTOM = "sr_tenka_glass_kokeibunsokuteinichiji_Bottom";
+
+    /**
+     * 正味重量計算(画面下ボタン)
+     */
+    public static final String BTN_SYOUMIJYUURYOU_KEISAN_BOTTOM = "sr_tenka_glass_syoumijyuuryou_keisan_Bottom";
+
+    /**
+     * 撹拌開始日時(画面下ボタン)
+     */
+    public static final String BTN_KAKUHANKAISINICHIJI_BOTTOM = "sr_tenka_glass_kakuhankaisinichiji_Bottom";
+
+    /**
+     * 撹拌終了日時(画面下ボタン)
+     */
+    public static final String BTN_KAKUHANSYUURYOUNICHIJI_BOTTOM = "sr_tenka_glass_kakuhansyuuryounichiji_Bottom";
+
+    /**
+     * 乾燥開始日時①(画面下ボタン)
+     */
+    public static final String BTN_KANSOUKAISINICHIJI1_BOTTOM = "sr_tenka_glass_kansoukaisinichiji1_Bottom";
+
+    /**
+     * 乾燥終了日時①(画面下ボタン)
+     */
+    public static final String BTN_KANSOUSYUURYOUNICHIJI1_BOTTOM = "sr_tenka_glass_kansousyuuryounichiji1_Bottom";
+
+    /**
+     * 乾燥開始日時②(画面下ボタン)
+     */
+    public static final String BTN_KANSOUKAISINICHIJI2_BOTTOM = "sr_tenka_glass_kansoukaisinichiji2_Bottom";
+
+    /**
+     * 乾燥終了日時②(画面下ボタン)
+     */
+    public static final String BTN_KANSOUSYUURYOUNICHIJI2_BOTTOM = "sr_tenka_glass_kansousyuuryounichiji2_Bottom";
+
+    /**
+     * 乾燥後重量計算(画面下ボタン)
+     */
+    public static final String BTN_KANSOUGOJYUURYOU_KEISAN_BOTTOM = "sr_tenka_glass_kansougojyuuryou_keisan_Bottom";
+
+    /**
+     * 固形分計算(画面下ボタン)
+     */
+    public static final String BTN_KOKEIBUN_KEISAN_BOTTOM = "sr_tenka_glass_kokeibun_keisan_Bottom";
+
+    /**
+     * 2回目撹拌開始日時(画面下ボタン)
+     */
+    public static final String BTN_KAKUHANKAISINICHIJI2_BOTTOM = "sr_tenka_glass_kakuhankaisinichiji2_Bottom";
+
+    /**
+     * 2回目撹拌終了日時(画面下ボタン)
+     */
+    public static final String BTN_KAKUHANSYUURYOUNICHIJI2_BOTTOM = "sr_tenka_glass_kakuhansyuuryounichiji2_Bottom";
+
+    /**
+     * 2回目乾燥開始日時①(画面下ボタン)
+     */
+    public static final String BTN_KANSOUKAISINICHIJI1_2_BOTTOM = "sr_tenka_glass_kansoukaisinichiji1_2_Bottom";
+
+    /**
+     * 2回目乾燥終了日時①(画面下ボタン)
+     */
+    public static final String BTN_KANSOUSYUURYOUNICHIJI1_2_BOTTOM = "sr_tenka_glass_kansousyuuryounichiji1_2_Bottom";
+
+    /**
+     * 2回目乾燥後重量計算(画面下ボタン)
+     */
+    public static final String BTN_KANSOUGOJYUURYOU2_KEISAN_BOTTOM = "sr_tenka_glass_kansougojyuuryou2_keisan_Bottom";
+
+    /**
+     * 2回目固形分計算(画面下ボタン)
+     */
+    public static final String BTN_KOKEIBUN2_KEISAN_BOTTOM = "sr_tenka_glass_kokeibun2_keisan_Bottom";
+
+    /**
+     * 仮登録(画面下ボタン)
+     */
+    public static final String BTN_KARI_TOUROKU_BOTTOM = "sr_tenka_glass_kari_touroku_Bottom";
+
+    /**
+     * 登録(画面下ボタン)
+     */
+    public static final String BTN_INSERT_BOTTOM = "sr_tenka_glass_insert_Bottom";
+
+    /**
+     * 修正(画面下ボタン)
+     */
+    public static final String BTN_UPDATE_BOTTOM = "sr_tenka_glass_update_Bottom";
+
+    /**
+     * 削除(画面下ボタン)
+     */
+    public static final String BTN_DELETE_BOTTOM = "sr_tenka_glass_delete_Bottom";
+
+    /**
+     * ユーザー認証パラメータ(修正)
+     */
+    public static final String USER_AUTH_UPDATE_PARAM = "sr_tenka_glass_update_button";
+
+    /**
+     * ユーザー認証パラメータ(削除)
+     */
+    public static final String USER_AUTH_DELETE_PARAM = "sr_tenka_glass_delete_button";
+}
